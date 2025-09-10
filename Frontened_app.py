d3b4744c5c9d611600ed3b3a697b78afd022869b
Dharvi <kdharvi01@gmail.com>
2025-09-10 15:22:27 +0530

    Update Frontened_app.py

---
+++ Frontened_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from chatbot_backend import GitaChatbot
+
+st.title("📖 Bhagavad Gita Chatbot")
+
+if "bot" not in st.session_state:
+    st.session_state.bot = GitaChatbot()
+
+query = st.text_input("Ask something about Bhagavad Gita:")
+
+if query:
+    verses, explanation = st.session_state.bot.chat(query)
+    if verses:
+        for v in verses:
+            st.write("📜 Verse:", v)
+    st.success(explanation)
